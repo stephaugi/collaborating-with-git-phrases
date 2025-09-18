dbaa74e73f5d9ab617ea62f362760fe16e4a5384
Stephanie Lin <slin.animation@gmail.com>
2025-09-18 14:48:13 -0400

    I put it back

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ def greet():
     print("Hello, World!")
 def farewell():
     print("Goodbye, World!")
+
+def check():
+    print("You ok?")
+
+def laugh():
+    print("Hahaha!")
+
+def new_conflict():
+    print('conflict')
+
+def another_conflict():
+    print('conflict!!!!')
